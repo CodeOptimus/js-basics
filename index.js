798b8bd2399a88b8edb0e6d294983e46ee99094e
Optimus Elikem <89908948+CodeOptimus@users.noreply.github.com>
2023-02-12 17:17:09 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function product(input1*input2);
+console.log(product)
+
+let firstName = "Portia" 
+let lastName = "Mawuena"
+let stuternTrack = "Frontend Development"
+let skills = "HTML, CSS, JavaScript"
+let favoriteColor = "Blue" 
+let yearOfIndependence = "1957"
+console.log(firstName + lastName + stuternTrack + skills + favoriteColor + yearOfIndependence);
